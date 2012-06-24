a4ab530b47ff23aed5480cd041852808a54f8bad
Martin Rue <hello@martinrue.com>
2012-06-24 15:06:52 +0100

    change tabs to spaces

--- routes/api.js
+++ routes/api.js
@@ -1,56 +1,56 @@
 var _ = require('underscore');
 
 var routes = function(app, processManager) {
-	app.post('/log', function(req, res) {
-		if (requiredParamsMissing(['name', 'message'], req, res)) return;
-		
-	  var process = processManager.getOrCreateProcess(req.body.name);
-	  process.addLog(req.body.message);
+  app.post('/log', function(req, res) {
+    if (requiredParamsMissing(['name', 'message'], req, res)) return;
+    
+    var process = processManager.getOrCreateProcess(req.body.name);
+    process.addLog(req.body.message);
 
-	  res.json({ status: true });
-	});
+    res.json({ status: true });
+  });
 
-	app.post('/set', function(req, res) {
-	  if (requiredParamsMissing(['name', 'key', 'value'], req, res)) return;
+  app.post('/set', function(req, res) {
+    if (requiredParamsMissing(['name', 'key', 'value'], req, res)) return;
 
-	  var process = processManager.getOrCreateProcess(req.body.name);
-	  process.setStat(req.body.key, req.body.value);
+    var process = processManager.getOrCreateProcess(req.body.name);
+    process.setStat(req.body.key, req.body.value);
 
-	  res.json({ status: true });
-	});
+    res.json({ status: true });
+  });
 
-	app.post('/inc', function(req, res) {
-	  if (requiredParamsMissing(['name', 'key'], req, res)) return;
+  app.post('/inc', function(req, res) {
+    if (requiredParamsMissing(['name', 'key'], req, res)) return;
 
-	  var process = processManager.getOrCreateProcess(req.body.name);
-	  process.incStat(req.body.key, req.body.by);
+    var process = processManager.getOrCreateProcess(req.body.name);
+    process.incStat(req.body.key, req.body.by);
 
-	  res.json({ status: true });
-	});
+    res.json({ status: true });
+  });
 
-	app.post('/dec', function(req, res) {
-	  if (requiredParamsMissing(['name', 'key'], req, res)) return;
+  app.post('/dec', function(req, res) {
+    if (requiredParamsMissing(['name', 'key'], req, res)) return;
 
-	  var process = processManager.getOrCreateProcess(req.body.name);
-	  process.decStat(req.body.key, req.body.by);
+    var process = processManager.getOrCreateProcess(req.body.name);
+    process.decStat(req.body.key, req.body.by);
 
-	  res.json({ status: true });
-	});
+    res.json({ status: true });
+  });
 
-	app.post('/removestat', function(req, res) {
-		if (requiredParamsMissing(['process', 'stat'], req, res)) return;
+  app.post('/removestat', function(req, res) {
+    if (requiredParamsMissing(['process', 'stat'], req, res)) return;
 
-		var process = processManager.getOrCreateProcess(req.body.process);
-		process.removeStat(req.body.stat);
+    var process = processManager.getOrCreateProcess(req.body.process);
+    process.removeStat(req.body.stat);
 
-		res.json({ status: true });
-	});
+    res.json({ status: true });
+  });
 
-	var requiredParamsMissing = function(keys, req, res) {
-		_.each(keys, function(key) {
-			if (!req.body[key]) res.json({ error: key + ' required' });
-		});
-	};
+  var requiredParamsMissing = function(keys, req, res) {
+    _.each(keys, function(key) {
+      if (!req.body[key]) res.json({ error: key + ' required' });
+    });
+  };
 };
 
 module.exports = routes;
--- routes/web.js
+++ routes/web.js
@@ -2,18 +2,18 @@ var _ = require('underscore');
 
 var routes = function(app, processManager) {
   app.get('/', function(req, res) {
-	  var locals = { 
+    var locals = { 
       snapshot: processManager.getProcessesSnapshot(),
       showlogo: app.enabled('showlogo'),
       _: _
     };
 
     res.render('index', { layout: false, locals: locals });
-	});
+  });
 
-	app.get('/snapshot', function(req, res) {
-		res.json(processManager.getProcessesSnapshot());
-	});
+  app.get('/snapshot', function(req, res) {
+    res.json(processManager.getProcessesSnapshot());
+  });
 };
 
 module.exports = routes;
