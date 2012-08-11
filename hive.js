2f951148be3205de504f68bf4ae378bd77685aa8
Michael Heap <m@michaelaheap.com>
2012-08-11 12:50:04 +0100

    fs.existsSync does not exist in Node < 0.8, it's path.existsSync. Load based on process.versions.node

--- hive.js
+++ hive.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = module.exports = express.createServer();
 var processManager = require('./lib/process-manager');
 
+var existsSync = (process.versions.node.substr(0,3) >= 0.8) ? require('fs').existsSync : require('path').existsSync;
+
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'html');
@@ -18,7 +20,7 @@ app.configure(function() {
 
 app.configure(function() {
   var logoPath = __dirname + '/public/img/logo.png';
-  if (fs.existsSync(logoPath)) app.enable('showlogo');
+  if (existsSync(logoPath)) app.enable('showlogo');
 });
 
 require('./routes/web')(app, processManager);
